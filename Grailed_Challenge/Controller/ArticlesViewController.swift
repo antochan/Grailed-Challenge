f020733c8d6d0e93287d9d3d7231630ae943d1d2
Antonio Chan <antonio.chan@emory.edu>
2018-11-05 02:19:57 -0500

    completed API request

---
+++ Grailed_Challenge/Controller/ArticlesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ArticlesViewController.swift
+//  Grailed_Challenge
+//
+//  Created by Antonio Chan on 2018/11/5.
+//  Copyright © 2018 Antonio Chan. All rights reserved.
+//
+
+import UIKit
+
+class ArticlesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Grailed_Challenge/Utilities/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Grailed_Challenge
+//
+//  Created by Antonio Chan on 2018/11/5.
+//  Copyright © 2018 Antonio Chan. All rights reserved.
+//
+
+import Foundation
